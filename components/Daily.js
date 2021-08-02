8d82f61e52c66c49037af75de128b9525e69a764
michaelrlai88 <michaelrlai88@gmail.com>
2021-08-02 01:53:15 -0700

    fixed api call

--- components/Daily.js
+++ components/Daily.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  background-color: lightseagreen;
+`;
 
 const Daily = () => {
-  return <div>Daily</div>;
+  return <Container>Daily</Container>;
 };
 
 export default Daily;
--- components/Hourly.js
+++ components/Hourly.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  background-color: lightskyblue;
+`;
 
 const Hourly = () => {
-  return <div>Hourly</div>;
+  return (
+    <Container>
+      <div>Hourly</div>
+    </Container>
+  );
 };
 
 export default Hourly;
