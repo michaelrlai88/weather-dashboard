c6fc58b65e0833ef1387e419d3b165f4ce75f017
michaelrlai88 <michaelrlai88@gmail.com>
2021-08-02 15:15:33 -0700

    fixed styler

--- pages/index.js
+++ pages/index.js
@@ -13,22 +13,15 @@ const Home = () => {
       <Dashboard />
 
       <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
         * {
           box-sizing: border-box;
+          margin: 0;
+          padding: 0;
+        }
+
+        body {
+          font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+            Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
         }
       `}</style>
     </div>
